1f81cb4d2154ce7f3aea7d9b81b2c76214e04de8
Apoorv Rane <apoorv.rane@neosoftmail.com>
2022-09-07 11:10:07 +0530

    Initial Commit

---
+++ src/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
+using TestFeatureFlags;
+using TestFeatureFlags.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration(config =>
+            {
+                var settings = config.Build();
+                var connection = settings.GetConnectionString("AppConfig");
+                config.AddAzureAppConfiguration(options =>
+                    options.Connect(connection).UseFeatureFlags(featureFlagOptions =>
+                    {
+                        featureFlagOptions.CacheExpirationInterval = TimeSpan.FromMinutes(5);
+                    }));
+            });
+
+// Add services to the container.
+builder.Services.AddAzureAppConfiguration();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddFeatureManagement()
+    .AddFeatureFilter<TargetingFilter>();
+builder.Services.AddSingleton<ITargetingContextAccessor, TestTargetingContextAccessor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAzureAppConfiguration();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
